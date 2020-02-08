08c7d44f1dd9b47d577c3a39bd48a0e3d7a9118f
AnamKhurshid17 <41837439+AnamKhurshid17@users.noreply.github.com>
2020-02-08 21:26:06 +0500

    minor updation

---
+++ build/precache-manifest.b6179d57feab062db859ed908dee9e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e711a2256e4a4ce9081fb93751f5a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a562a8e3cd7f5a7f1b8",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1cf58ff5e920dcd0317a",
+    "url": "/static/css/main.32334cc6.chunk.css"
+  },
+  {
+    "revision": "1a562a8e3cd7f5a7f1b8",
+    "url": "/static/js/2.6e7cf8c2.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/static/js/2.6e7cf8c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cf58ff5e920dcd0317a",
+    "url": "/static/js/main.4dcf582f.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
